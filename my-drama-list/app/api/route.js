eedbf833c8c2c84777cc04b8a51b39fc408595a2
vishwajeeta <gs699542@gmail.com>
2024-02-04 05:40:34 +0530

    solved one error

--- my-drama-list/app/api/route.js
+++ my-drama-list/app/api/route.js
@@ -1,6 +1,18 @@
 import { NextResponse } from "next/server";
 
+const fs =require( 'fs');
+const path =require( 'path');
+const jsonFilePath = path.resolve('app/api/data/dramas.json');
+
 export const  GET = async(request)=>{
-    return new NextResponse.json({body:"vishwa"});
+    try{
+        const data = fs.readFileSync(jsonFilePath, 'utf-8');
+        console.log(data)
+        return  NextResponse.json(data);
+
+    } catch(err){
+        console.log('Error reading json file:',err);
+        return NextResponse.json([]);
+    }
 }
  
